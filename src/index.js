b40a68fd5f624ebd947f466136307014686a64db
Mariolast <45509800+Mariolast@users.noreply.github.com>
2022-01-09 13:49:13 +0200

    fix: Update test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function toReadable (number) {
-  
+  const onenumber = ['','one','two','three',
+    'four','five','six','seven','eight','nine','ten', 
+    'eleven','twelve','thirteen','fourteen','fifteen','sixteen',
+    'seventeen','eighteen','nineteen','twenty'];
+    const twonumber = ['','','twenty','thirty','forty','fifty',
+    'sixty','seventy','eighty','ninety'];
+    const threenumber = ['','one hundred','two hundred','three hundred',
+    'four hundred','five hundred','six hundred','seven hundred',
+    'eight hundred','nine hundred'];
+    numberstr = number.toString()
+    const numberstrA = numberstr.split('')
+    if (number <= 20) {result = twonumber[number];console.log(number,onenumber[number])}                            
+    if (number > 19 && number <= 99) {
+            const numberarr = numberstr.split('')
+             result = twonumber[numberarr[0]] + ' ' + onenumber[numberarr[1]]}
+    if (number > 99) {
+                const numberarr = numberstr.split('')
+                 result = threenumber[numberarr[0]] + " "+ twonumber[numberarr[1]] + ' ' + onenumber[numberarr[2]]}
+    return result  
 }
